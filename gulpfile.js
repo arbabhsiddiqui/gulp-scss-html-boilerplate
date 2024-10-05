edd3a4d1c7cac8a140bd4c23a1b492d566f10ab1
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2024-10-05 15:14:52 +0530

    update glup workflow to reduce hot reload time during development and add new npm script for production build and for updating images separately

--- gulpfile.js
+++ gulpfile.js
@@ -1,151 +1,202 @@
-const gulp = require("gulp");
-const { src, series, parallel, dest, watch } = require("gulp");
+// const gulp = require('gulp');
+const { src, series, parallel, dest, watch } = require('gulp');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const rollup = require('gulp-better-rollup');
+const babel = require('rollup-plugin-babel');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const terser = require('gulp-terser');
+const imagemin = require('gulp-imagemin');
+const imageWebp = require('gulp-webp');
+const fileInclude = require('gulp-file-include');
+const browsersync = require('browser-sync').create();
 
-//* importing gulp plugins
-
-const concat = require("gulp-concat");
-const sourcemaps = require("gulp-sourcemaps");
-const autoprefixer = require("autoprefixer");
-
-//* scss plugins
-const sass = require("gulp-sass")(require("sass"));
-const postcss = require("gulp-postcss");
-const cssnano = require("cssnano");
-
-//* html plugins
-const fileInclude = require("gulp-file-include");
-
-//* image plugin
-const imagemin = require("gulp-imagemin");
-const imageWebp = require("gulp-webp");
-
-//* js plugin
-const rollup = require("gulp-better-rollup");
-const babel = require("rollup-plugin-babel");
-const resolve = require("rollup-plugin-node-resolve");
-const commonjs = require("rollup-plugin-commonjs");
-const terser = require("gulp-terser");
-
-//* create local sever
-const browsersync = require("browser-sync").create();
-
-//! define paths for source and destination
-const SOURCE = "src";
-const DIST = "dist";
+const SOURCE = 'src';
+const DEV = 'dev';
+const PROD = 'dist';
 
 const SRC_JS_PATH = `${SOURCE}/js/**/**/*.js`;
 const SRC_HTML_PATH = `${SOURCE}/*.html`;
-const DIST_JS_PATH = `${DIST}/js`;
 const SRC_CSS_PATH = `${SOURCE}/scss/**/*.scss`;
-const DIST_CSS_PATH = `${DIST}/css`;
 const SRC_IMAGE_PATH = `${SOURCE}/images/**/*.{jpg,png,mp4}`;
-const DIST_IMAGE_PATH = `${DIST}/images`;
 
-//! combine html
-function combineHtml() {
+const DEV_JS_PATH = `${DEV}/js`;
+const DEV_CSS_PATH = `${DEV}/css`;
+const DEV_IMAGE_PATH = `${DEV}/images`;
+
+const PROD_JS_PATH = `${PROD}/js`;
+const PROD_CSS_PATH = `${PROD}/css`;
+const PROD_IMAGE_PATH = `${PROD}/images`;
+
+/* ******************************* 
+html task block
+***************************************** */
+// Combine HTML files
+
+function combineHtmlTaskForDevelopment() {
   return src(SRC_HTML_PATH)
-    .pipe(
-      fileInclude({
-        prefix: "@@",
-        basepath: "@file",
-      })
-    )
-    .pipe(gulp.dest(DIST));
+    .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
+    .pipe(dest(DEV));
 }
 
-//* compress images and copy them into dist/images folder
+function combineHtmlTaskForProduction() {
+  return src(SRC_HTML_PATH)
+    .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
+    .pipe(dest(PROD));
+}
+
+/* ******************************* 
+html task block end
+***************************************** */
+
+/* ******************************* 
+image compression task block
+***************************************** */
+
+// compresses images
 function imgCompressionTask() {
   return src(SRC_IMAGE_PATH)
     .pipe(imagemin())
-    .pipe(gulp.dest(`${SOURCE}/compressImage`));
+    .pipe(dest(`${SOURCE}/compressImage`));
 }
 
-//* convert Them into webp images !!Folder Location dist/images
-function convertImagesIntoWebPeTask() {
+function convertImagesIntoWebPImagesTaskForDevelopment() {
   return src(`${SOURCE}/compressImage/**/*`)
     .pipe(imageWebp())
-    .pipe(gulp.dest(DIST_IMAGE_PATH));
+    .pipe(dest(DEV_IMAGE_PATH));
 }
 
-//* minify js and copy them into  dist/js folder
-function jsTask() {
-  return src(SRC_JS_PATH)
-    .pipe(rollup({ plugins: [babel(), resolve(), commonjs()] }, "umd"))
-    .pipe(sourcemaps.init())
-    .pipe(concat("main.js"))
-    .pipe(terser())
-    .pipe(sourcemaps.write("."))
-    .pipe(dest(DIST_JS_PATH));
+function convertImagesIntoWebPImagesTaskForProduction() {
+  return src(`${SOURCE}/compressImage/**/*`)
+    .pipe(imageWebp())
+    .pipe(dest(PROD_IMAGE_PATH));
 }
 
-// *convert scss to css then minify then  copy them into  dist/css folder
-function cssTask() {
+/* ******************************* 
+image compression task block end
+***************************************** */
+
+/* ******************************* 
+css task block
+***************************************** */
+
+// Compile SCSS to CSS, create sourcemaps for development
+function cssTaskForDevelopment() {
   return src(SRC_CSS_PATH)
     .pipe(sass())
     .pipe(sourcemaps.init())
-    .pipe(concat("style.css"))
-    .pipe(postcss([autoprefixer(), cssnano()])) //not all plugins work with postcss only the ones mentioned in their documentation
-    .pipe(sourcemaps.write("."))
-    .pipe(dest(DIST_CSS_PATH));
+    .pipe(concat('style.css'))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(DEV_CSS_PATH));
 }
 
-//* BrowserSync Server setup a sever for live reload
+// Compile SCSS to CSS, minify for production
+function cssTaskForProduction() {
+  return src(SRC_CSS_PATH)
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest(PROD_CSS_PATH));
+}
+
+/* ******************************* 
+css task block end
+***************************************** */
+
+/* ******************************* 
+js task block
+***************************************** */
+
+// Bundle JavaScript with Rollup, create sourcemaps for development
+function jsTaskForDevelopment() {
+  return src(SRC_JS_PATH)
+    .pipe(rollup({ plugins: [babel(), resolve(), commonjs()] }, 'umd'))
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(DEV_JS_PATH));
+}
+
+// Bundle JavaScript with Rollup, minify for production
+function jsTaskForProduction() {
+  return src(SRC_JS_PATH)
+    .pipe(rollup({ plugins: [babel(), resolve(), commonjs()] }, 'umd'))
+    .pipe(concat('main.js'))
+    .pipe(terser())
+    .pipe(dest(PROD_JS_PATH));
+}
+
+/* ******************************* 
+js task block  end
+***************************************** */
+
+// Setup BrowserSync server for live reload
 function browserSyncServe(cb) {
   browsersync.init({
-    server: {
-      baseDir: "./dist/",
-    },
-    notify: {
-      styles: {
-        top: "auto",
-        bottom: "0",
-      },
-    },
+    server: { baseDir: `${DEV}` },
+    notify: { styles: { top: 'auto', bottom: '0' } },
   });
   cb();
 }
 
-//*  listen for changes in files
+// Reload BrowserSync server
 function browserSyncReload(cb) {
   browsersync.reload();
   cb();
 }
 
-//*  watch changes in files
+// Watch for file changes and trigger tasks
 function watchTask() {
-  //*  watch changes occur in html files
   watch(
-    ["src/*.html", "./src/templates/*.html"],
+    ['src/*.html', './src/templates/*.html'],
     { interval: 1000 },
-    series(combineHtml, browserSyncReload)
+    series(combineHtmlTaskForDevelopment, browserSyncReload)
   );
-  //* changes in images
-  watch("src/images/**/*.{jpg,png}", imgCompressionTask);
-
-  watch("dist/images/**/*.{jpg,png}", convertImagesIntoWebPeTask);
-  //* watch changes occur in scss and js
   watch(
     [SRC_CSS_PATH, SRC_JS_PATH],
     { interval: 1000 },
-    series(cssTask, jsTask, browserSyncReload)
+    series(cssTaskForDevelopment, jsTaskForDevelopment, browserSyncReload)
   );
 }
 
-//* exporting functions
-exports.cssTask = cssTask;
-exports.jsTask = jsTask;
-exports.imgCompressionTask = imgCompressionTask;
-exports.convertImagesIntoWebPeTask = convertImagesIntoWebPeTask;
-
-//* setup default task generate dist and wait for changes
+// Default task for development
 exports.default = series(
   parallel(
-    combineHtml,
-    imgCompressionTask,
-    convertImagesIntoWebPeTask,
-    jsTask,
-    cssTask
+    combineHtmlTaskForDevelopment,
+    cssTaskForDevelopment,
+    jsTaskForDevelopment
   ),
+  imgCompressionTask,
+  convertImagesIntoWebPImagesTaskForDevelopment,
   browserSyncServe,
   watchTask
+);
+
+// Production tasks
+exports.build = series(
+  parallel(
+    combineHtmlTaskForProduction,
+    cssTaskForProduction,
+    jsTaskForProduction
+  ),
+  imgCompressionTask,
+  convertImagesIntoWebPImagesTaskForProduction
+);
+
+// Compress images for `development`
+exports.compressImagesDev = series(
+  imgCompressionTask,
+  convertImagesIntoWebPImagesTaskForDevelopment
+);
+
+// Compress images for `production`
+exports.compressImagesBuild = series(
+  imgCompressionTask,
+  convertImagesIntoWebPImagesTaskForProduction
 );
